2db1acea698a3cf796d8ae50c551fd40ed1978b0
Валерия Патина <65231028+devilishgrapefruit@users.noreply.github.com>
2022-06-05 08:13:42 +0300

    Fix bags

--- src/main/java/com/projects/CoffeeWorld/services/EmailService.java
+++ src/main/java/com/projects/CoffeeWorld/services/EmailService.java
@@ -1,6 +1,5 @@
 package com.projects.CoffeeWorld.services;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.mail.javamail.JavaMailSender;
 
 
-@Slf4j
 @Service
 public class EmailService {
     private final JavaMailSender javaMailSender;
